bd3a7715ee86c3817a80f23de154b41b0d18cc4a
beta <santibeta82@gmail.com>
2023-04-16 14:26:15 -0500

    Improved performance

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -5,7 +5,7 @@
             <div class="flex items-center justify-center flex-wrap gap-8">
                 <div v-for="project in projects()" :key="project.title"
                     class="shadow-md bg-bgLightSecondary dark:bg-bgDarkSecondary border border-transparent dark:border-sky-900 max-w-xs rounded-xl overflow-hidden">
-                    <img :src="getImgUrl(project.img)" alt="project image">
+                    <img :src="getImgUrl(project.img)" alt="project image" width="320" height="240">
                     <div class="p-5 flex flex-col gap-5 border-t-2 border-primary">
                         <div class="flex flex-col gap-1">
                             <h3 class="text-primary text-2xl font-semibold">{{ project.title }}</h3>
